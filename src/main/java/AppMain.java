a1c95f5fbc7c02034a6762665154e99193091914
Quang Linh <linhql1978@gmail.com>
2020-09-12 17:40:40 +0700

    branch#5 hello

--- src/main/java/AppMain.java
+++ src/main/java/AppMain.java
@@ -1,7 +1,9 @@
 public class AppMain {
     public static void main(String[] args) {
         System.out.println("abc");
+        System.out.println("Hello I'm from branch#5");
         System.out.println("branch#1");
+        System.out.println("branch#5: "+"hihi");
         System.out.println("branch xyz");
     }
 }
