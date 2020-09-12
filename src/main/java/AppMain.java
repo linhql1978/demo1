be4a53990dca0ffa8b5e58e8ac56866515c42527
Quang Linh <linhql1978@gmail.com>
2020-09-12 17:45:13 +0700

    branch#6 modify

--- src/main/java/AppMain.java
+++ src/main/java/AppMain.java
@@ -2,6 +2,7 @@ public class AppMain {
     public static void main(String[] args) {
         System.out.println("abc");
         System.out.println("branch#1");
-        System.out.println("branch xyz");
+        System.out.println("branch#6 hello");
+        System.out.println("branch xyz"+";branch#6 modify");
     }
 }
