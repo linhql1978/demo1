8f14c4832fe3b5ccaf0a925868eb45271149b3f2
Quang Linh <linhql1978@gmail.com>
2020-09-12 19:04:13 +0700

    I'm tonight#1

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        String str = "branch#8";
+        String str = "branch#8"+"I'm tonight#1";
         String str1 = "I'm tonight2";
         System.out.println(str1+str);
     }
